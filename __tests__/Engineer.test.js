cd544179f3ae58e77a657da24ae27d1dcfee0d42
Matt McGee <mlmcgeenc@gmail.com>
2022-06-16 11:51:35 -0400

    write engineer class to pass tests

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -4,6 +4,7 @@ test('checks engineer specific properties', () => {
 	const engineer = new Engineer('John', '007', 'john@email.com', 'github-username');
 
 	expect(engineer.github).toBe('github-username');
-	expect(engineer.getGithub).toBe('github-username');
-	expect(engineer.getRole).toBe('Engineer');
+  expect(engineer).toHaveProperty('getGithub');
+	expect(engineer.getGithub()).toBe(engineer.github);
+	expect(engineer.getRole()).toBe('Engineer');
 });
