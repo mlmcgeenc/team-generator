f0e6e2d0337df0d712575f1fb3cc9441164577f7
Matt McGee <mlmcgeenc@gmail.com>
2022-06-16 14:36:11 -0400

    write manager class to pass tests

--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -1,8 +1,8 @@
-const Manager = require('../lib/Manager')
+const Manager = require('../lib/Manager');
 
 test('checks Manager specific properties and methods', () => {
-  const manager = new Manager('John', '007', 'john@email.com', '314');
+	const manager = new Manager('John', '007', 'john@email.com', '314');
 
-  expect(manager.officeNumber).toBe('314')
-  expect(manager.getRole).toBe('Manager')
-})
+	expect(manager.officeNumber).toBe('314');
+	expect(manager.getRole()).toBe('Manager');
+});
