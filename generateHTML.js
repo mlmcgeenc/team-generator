f9772137ca0db35681449dde66e1c3ac98c99a2d
Matt McGee <mlmcgeenc@gmail.com>
2022-06-27 18:26:39 -0400

    build cards from data, save html, style cards

---
+++ generateHTML.js
@@ -0,0 +1,94 @@
+const fs = require('fs');
+
+const createManagerCard = (managerInfo) => {
+	console.log(managerInfo);
+	return `
+  <div class="card shadow m-3" style="width: 18rem;">
+    <div class="card-header bg-primary text-light">
+      <h3>${managerInfo.name}</h3>
+      <h5><i class="fa-solid fa-mug-hot"></i> Manager</h5>
+    </div>
+    <div class="card-body bg-light">
+      <ul class="list-group">
+        <li class="list-group-item">ID: ${managerInfo.id}</li>
+        <li class="list-group-item">Email: <a class="card-link text-nowrap" href="mailto:${managerInfo.email}">${managerInfo.email}</a></li>
+        <li class="list-group-item">Office Number: ${managerInfo.officeNumber}</li>
+      </ul>
+    </div>
+  </div>
+  `;
+};
+
+const createEngineerCards = (engineerList) => {
+	return engineerList
+		.map((engineer) => {
+			return `
+    <div class="card shadow m-3" style="width: 18rem;">
+    <div class="card-header bg-primary text-light">
+      <h3>${engineer.name}</h3>
+      <h5><i class="fa-solid fa-glasses"></i> Engineer</h5>
+    </div>
+    <div class="card-body bg-light">
+      <ul class="list-group">
+        <li class="list-group-item">ID: ${engineer.id}</li>
+        <li class="list-group-item">Email: <a class="card-link text-nowrap" href="mailto:${engineer.email}">${engineer.email}</a></li>
+        <li class="list-group-item">Github: <a class="card-link" href="https://github.com/${engineer.github}">${engineer.github}</a></li>
+      </ul>
+    </div>
+  </div>
+    `;
+		})
+		.join('');
+};
+
+const createInternCards = (internList) => {
+	return internList
+		.map((intern) => {
+			return `
+    <div class="card shadow m-3" style="width: 18rem;">
+    <div class="card-header bg-primary text-light">
+      <h3>${intern.name}</h3>
+      <h5><i class="fa-solid fa-graduation-cap"></i> Intern</h5>
+    </div>
+    <div class="card-body bg-light">
+      <ul class="list-group">
+        <li class="list-group-item">ID: ${intern.id}</li>
+        <li class="list-group-item">Email: <a class="card-link text-nowrap" href="mailto:${intern.email}">${intern.email}</a></li>
+        <li class="list-group-item">School: ${intern.school}</li>
+      </ul>
+    </div>
+  </div>
+    `;
+		})
+		.join('');
+};
+
+function generateHTML(company) {
+	let page = `
+  <!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css">
+</head>
+<body>
+<div class="container card-group">
+  ${createManagerCard(company.manager)}
+  ${createEngineerCards(company.engineers)}
+  ${createInternCards(company.interns)}
+</div>
+</body>
+<script src="https://kit.fontawesome.com/c20b00daf3.js" crossorigin="anonymous"></script>
+</html>
+`;
+	try {
+		fs.writeFileSync('index.html', page);
+	} catch (err) {
+		console.error(err);
+	}
+}
+
+module.exports = generateHTML;
--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const inquirer = require('inquirer');
+const fs = require('fs');
+const generateHTML = require('./generateHTML');
 const Choices = require('inquirer/lib/objects/choices');
 const Manager = require('./lib/Manager');
 const Engineer = require('./lib/Engineer');
@@ -35,7 +37,7 @@ const newInternQuestion = [
 		type: 'input',
 		name: 'school',
 		message: 'What is the name of their school?',
-    default: 'Local-University',
+		default: 'Local-University',
 	},
 ];
 
@@ -44,26 +46,9 @@ const nextStepQuestion = [
 		type: 'list',
 		name: 'nextStep',
 		message: 'What would you like to do next?',
-		choices: ['Add a new engineer to my team.', 'Add a new intern to my team.', new inquirer.Separator(), 'Nothing. I am finished building my team.'],
+		choices: ['Add a new engineer to my team', 'Add a new intern to my team', new inquirer.Separator(), 'Nothing. I am finished building my team'],
 	},
 ];
-
-const nextStep = (answer) => {
-	console.log('nextStep >>> ', answer.nextStep);
-	switch (answer.nextStep) {
-		case 'Add a new engineer to my team.':
-			company.employeeType = 'engineer';
-			makeNewEmployee();
-			break;
-		case 'Add a new intern to my team.':
-			company.employeeType = 'intern';
-			makeNewEmployee();
-			break;
-		case 'Nothing. I am finished building my team.':
-			endTeamBuilding();
-			break;
-	}
-};
 
 const makeNewEmployee = (type) => {
 	const employeeQuestions = [
@@ -130,82 +115,48 @@ const makeNewEmployee = (type) => {
 	});
 };
 
+const nextStep = (answer) => {
+	switch (answer.nextStep) {
+		case 'Add a new engineer to my team':
+			company.employeeType = 'engineer';
+			makeNewEmployee();
+			break;
+		case 'Add a new intern to my team':
+			company.employeeType = 'intern';
+			makeNewEmployee();
+			break;
+		case 'Nothing. I am finished building my team':
+			endTeamBuilding();
+			break;
+	}
+};
+
 const makeNewManager = (name, id, email, special) => {
 	company.manager = new Manager(name, id, email, special);
 	company.lastIdCreated = id++;
 	company.lastOfficeAssigned = special++;
-	console.log(company);
-	//  TODO update local storage
 };
 
 const makeNewEngineer = (name, id, email, special) => {
 	company.engineers.push(new Engineer(name, id, email, special));
 	company.lastIdCreated = id++;
-	console.log(company);
 };
 
 const makeNewIntern = (name, id, email, special) => {
 	company.interns.push(new Intern(name, id, email, special));
 	company.lastIdCreated = id++;
-	console.log(company);
 };
 
 const endTeamBuilding = () => {
-	//  TODO save company to local storage and create html doc
-	console.log('endTeamBuilding');
-	console.log(company);
+	console.log('Finished building the team');
+  console.log(company)
+  generateHTML(company)
 };
 
 function init() {
-	if (!company.manager) {
-		company.employeeType = 'manager';
-		makeNewEmployee();
-
-		// const employeeQuestions = [
-		// 	{
-		// 		type: 'input',
-		// 		name: 'name',
-		// 		message: `Enter the name of your ${company.employeeType}`,
-		// 		validate: (name) => {
-		// 			if (name) {
-		// 				return true;
-		// 			} else {
-		// 				console.log(`${company.employeeType} name is required`);
-		// 				return false;
-		// 			}
-		// 		},
-		// 	},
-		// 	{
-		// 		type: 'input',
-		// 		name: 'id',
-		// 		message: `Enter the ${company.employeeType}'s id number`,
-		// 		default: company.lastIdCreated + 1,
-		// 	},
-		// 	{
-		// 		type: 'input',
-		// 		name: 'email',
-		// 		message: `Enter the ${company.employeeType}'s email address`,
-		// 		default: (answers) => `${answers.name}@our-company.com`,
-		// 		validate: (email) => {
-		// 			if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)) {
-		// 				return true;
-		// 			} else {
-		// 				console.log('A valid email address is required');
-		// 				return false;
-		// 			}
-		// 		},
-		// 	},
-		// ];
-		// .then(() => {
-		//   inquirer.prompt(nextStepQuestion).then(({ nextStep }) => {
-		//     if (nextStep === 'Nothing. I am finished building my team.') {
-		//       endTeamBuilding();
-		//     } else {
-		//       makeNewEmployee(nextStep);
-		//     }
-		//   });
-		// })
-	}
+  console.log('Begin by entering a manager for your team!');
+  company.employeeType = 'manager';
+  makeNewEmployee()
 }
 
 init();
